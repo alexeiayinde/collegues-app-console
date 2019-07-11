622a660df77a0b25b06fe7ee2d14dbbd0f5ccd9e
Alexei AYINDE <alexei.ayinde@gmail.com>
2019-07-11 17:32:35 +0200

    Mise-à-jour

--- js/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log('** Administration collègues **');
-var presentation_1 = require("./presentation");
-presentation_1.start();
